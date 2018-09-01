140c1639096a8dac1b64a8c1d33517378a5c04e9
Julius Cebreros <me@juliuscebreros.com>
2018-09-01 15:35:00 +1200

    Test: show start/reset in header

--- components/Header.test.js
+++ components/Header.test.js
@@ -11,4 +11,18 @@ describe("Header Component", () => {
     const link = header.find(Link);
     expect(link.prop("href")).toBe("/");
   });
+
+  it("should show reset button if activeChat", () => {
+    const header = shallow(<Header activeChat={true} />);
+
+    expect(header.find("#resetBtn").length).toBe(1);
+    expect(header.find("#startBtn").length).toBe(0);
+  });
+
+  it("should show start button if !activeChat", () => {
+    const header = shallow(<Header activeChat={false} />);
+
+    expect(header.find("#resetBtn").length).toBe(0);
+    expect(header.find("#startBtn").length).toBe(1);
+  });
 });
