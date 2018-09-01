6b9f299e2f214cf0c19a7495429a88509f2b6a1b
Julius Cebreros <me@juliuscebreros.com>
2018-09-01 16:02:32 +1200

    Clean up unused deps

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,7 @@
-import { shallow, mount } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
-import renderer from "react-test-renderer";
 
 import Index from "../pages/index.js";
-import Header from "../components/Header";
 
 describe("Index Page", () => {
   it("should show start button", () => {
