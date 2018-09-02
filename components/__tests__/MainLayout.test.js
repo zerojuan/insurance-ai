c18a83275e75126f864507a7dbf622ba708ad457
Julius Cebreros <me@juliuscebreros.com>
2018-09-02 19:26:12 +1200

    Move tests to __tests__

---
+++ components/__tests__/MainLayout.test.js
@@ -0,0 +1,20 @@
+import { shallow } from "enzyme";
+
+import MainLayout from "./MainLayout";
+import Header from "./Header";
+
+describe("MainLayout", () => {
+  it("should have a Header", () => {
+    const layout = shallow(<MainLayout />);
+    expect(layout.find(Header).length).toBe(1);
+  });
+
+  it("should render contents", () => {
+    const layout = shallow(
+      <MainLayout>
+        <p>Content</p>
+      </MainLayout>
+    );
+    expect(layout.find("p").text()).toBe("Content");
+  });
+});
