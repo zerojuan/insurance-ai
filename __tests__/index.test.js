cef34f51df31d43f6ce68eecf5f5579f142b47a6
Julius Cebreros <me@juliuscebreros.com>
2018-09-02 19:46:53 +1200

    Test: index calls chat/start api

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,31 @@
 import { mount } from "enzyme";
 import React from "react";
 
+import mockAxios from "jest-mock-axios";
+
 import Index from "../pages/index.js";
 
 describe("Index Page", () => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
   it("should show start button", () => {
     const index = mount(<Index />);
 
     expect(index.find("a#startBtn").length).toBe(1);
+  });
+
+  it("should start the chat on start clicked", () => {
+    const index = mount(<Index />);
+    index
+      .find("a#startBtn")
+      .first()
+      .simulate("click");
+
+    // called the mock api
+    expect(mockAxios.post).toHaveBeenCalledWith("/chat/start");
+
+    // then moved to next route
   });
 });
